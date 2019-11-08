1be0cd2cb85bbb93606ec96ab01410ba29caced2
Tsubasa Nakayama <app.nakayama@gmail.com>
2019-11-08 23:46:46 +0900

    add docker

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:10
+
+WORKDIR /usr/src/app
+
+COPY package.json ./
+COPY tsconfig.json ./
+COPY yarn.lock ./
+COPY /src/ ./src/
+
+RUN apt-get update && apt-get install -y yarn
+RUN yarn install && yarn build
+
+COPY . .
+
+EXPOSE 80
+
+CMD ["node", "dist/server.js"]
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 
 const app = express();
+const PORT = process.env.PORT || 80
 
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.send('Hello\n');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("start server : http://localhost:3000")
 });
